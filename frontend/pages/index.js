831371a5f0c2a48608b419b73063825577f0bf46
aproskam <ap@rskm.nl>
2021-09-07 15:52:23 +0200

    add project pages, add Page component, add _document page

---
+++ frontend/pages/index.js
@@ -0,0 +1,8 @@
+export default function IndexPage() {
+  return (
+    <div>
+      <p>Hello world!</p>
+      <code>Welcome</code>
+    </div>
+  );
+}
---
+++ frontend/pages/sell.js
@@ -0,0 +1,7 @@
+export default function SellPage() {
+  return (
+    <div>
+      <p>Hello world SELL!</p>
+    </div>
+  );
+}
